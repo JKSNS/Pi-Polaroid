1cfdb9cbd85bf55a84593104965b6d8f1a18f86a
Jackson Stephens <156037195+JKSNS@users.noreply.github.com>
2024-09-09 16:33:03 -0600

    Update main.c

--- display/main.c
+++ display/main.c
@@ -8,30 +8,26 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-// Define an array of colors
 #define NUMBER_OF_COLORS 5
 uint16_t COLORS[NUMBER_OF_COLORS] = {RED, GREEN, BLUE, YELLOW, CYAN};
 
-// Signal handler for graceful exit
 void intHandler(int dummy) {
     display_exit();
     exit(0);
 }
 
 int main(void) {
-    // Setup signal handler
     signal(SIGINT, intHandler);
 
-    // Initialize peripherals
     display_init();
     buttons_init();
     log_set_level(LOG_INFO);
 
     while (true) {
-        delay_ms(200); // Small delay to debounce button presses
+        delay_ms(200);
 
         if (button_up() == 0) {
-            display_clear(WHITE); // Clear the screen to white
+            display_clear(WHITE);
 
             display_draw_rectangle(0, 0, DISPLAY_WIDTH, 20, GREEN, true, 1);
 
@@ -50,20 +46,17 @@ int main(void) {
             poundPosX -= circleRadius - poundWidth / 2;
             poundPosY -= circleRadius - poundHeight / 2;
 
-            // Draw horizontal lines
             display_draw_line(poundPosX + 5, centerY - 10, poundPosX + poundWidth + 5, centerY - 10,
-                              BLACK, 3); // Horizontal line on top
+                              BLACK, 3);
             display_draw_line(poundPosX + 5, centerY + 10, poundPosX + poundWidth + 5, centerY + 10,
                               BLACK, 3);
-            // Draw vertical lines
             display_draw_line(centerX - 10, poundPosY + 5, centerX - 10,
                               poundPosY + poundHeight + 5, BLACK,
-                              3); // Vertical line on left
+                              3);
             display_draw_line(centerX + 10, poundPosY + 5, centerX + 10,
                               poundPosY + 5 + poundHeight, BLACK,
-                              3); // Vertical line on left
+                              3);
 
-            // Yellow circle
             display_draw_circle(centerX, centerY, circleRadius, YELLOW, false, 3);
 
         } else if (button_down() == 0) {
@@ -77,21 +70,18 @@ int main(void) {
         } else if (button_left() == 0) {
             display_clear(WHITE);
             const sFONT *fonts[5] = {&Font8, &Font12, &Font16, &Font20, &Font24};
-            int posY = 10; // Start position for Y
+            int posY = 10;
 
-            // Draw numbers with varying font sizes and colors
             for (int i = 0; i < 10; i++) {
-                char buffer[2] = {'0' + i, '\0'}; // Convert number to string
+                char buffer[2] = {'0' + i, '\0'};
                 display_draw_string(10 + i * (DISPLAY_WIDTH / 10), posY, buffer,
                                     (sFONT *)fonts[i % 5], COLORS[i % NUMBER_OF_COLORS], WHITE);
             }
 
-            posY += 25; // Adjust position for characters, assuming previous font size max 24px
-                        // height + 1px gap
+            posY += 25;
 
-            // Draw characters A-J with varying font sizes and colors
             for (int i = 0; i < 10; i++) {
-                char buffer[2] = {'A' + i, '\0'}; // Character A-J
+                char buffer[2] = {'A' + i, '\0'};
                 display_draw_string(10 + i * (DISPLAY_WIDTH / 10), posY, buffer,
                                     (sFONT *)fonts[i % 5], COLORS[i % NUMBER_OF_COLORS], WHITE);
             }
